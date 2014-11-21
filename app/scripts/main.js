ae4989b95158508d5d3eee47959ec54a24d299ca
Nick Dandakis <n.dandakis@bigspaceship.com>
2014-11-21 18:36:22 -0500

    This is the first commit. I was just playing before

---
+++ app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global Hackday, $*/
+
+
+window.Hackday = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    Hackday.init();
+});
