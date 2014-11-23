687e3b5b4e592940d45d580bfbc9e2a670a3d136
Nick Dandakis <n.dandakis@bigspaceship.com>
2014-11-23 07:20:45 -0500

    AM commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -173,7 +173,7 @@ module.exports = function (grunt) {
                 files: [{
                     expand: true,
                     cwd: '<%= yeoman.app %>/images',
-                    src: '{,*/}*.{png,jpg,jpeg}',
+                    src: '{,*/}*.{png,jpg,jpeg,gif}',
                     dest: '<%= yeoman.dist %>/images'
                 }]
             }
@@ -329,8 +329,6 @@ module.exports = function (grunt) {
     ]);
 
     grunt.registerTask('default', [
-        'jshint',
-        'test',
         'build'
     ]);
 };
--- app/scripts/router.js
+++ app/scripts/router.js
@@ -1,14 +1,29 @@
 'use strict';
 
 var Router = Backbone.Router.extend({
+    // content: '',
+
     initialize: function(){
         this.on('route', this.onRoute);
+    },
+
+    onRoute: function(){
+        $('#slidemenu li').removeClass('active');
+
+        var frag = Backbone.history.fragment;
+        if(frag.indexOf('create/') > -1){
+            $('#create-navigation-link').parent().addClass('active');
+        } else {
+            $(".navbar-toggle").trigger('click');
+            $('#'+Backbone.history.fragment+'-navigation-link').parent().addClass('active');
+        }
     },
 
     routes: {
         "": "explore",
         "explore": "explore",
-        "create": "create",
+        "create(/:type)": "create",
+        "register/:major/:minor": "registerPillar",
         "profile": "profile",
         "mybeacons": "mybeacons",
         "settings": "settings",
@@ -35,21 +50,38 @@ var Router = Backbone.Router.extend({
 
     },
 
-    create: function(){
+    create: function(type){
         console.log("Router: create");
         estimote.onUpdate = function(){};
 
-        var template = JST['app/scripts/templates/selectContent.hbs'];
-
+        var template;
+        if(type == undefined){
+            template = JST['app/scripts/templates/selectContent.hbs'];
+        } else {
+            template = JST['app/scripts/templates/create' + type.charAt(0).toUpperCase() + type.slice(1) + '.hbs'];
+        }
         $('#main').html(template());
+
+        $('#text-upload-button').on('click', function(e){
+            var text = $(e.target).prev().val();
+
+            var model = {
+                text: text
+            };
+
+            router.content = model;
+            router.recognizePillar();
+        });
     },
 
     profile: function(){
         console.log("Router: profile");
+        alert('Not ready yet');
     },
 
     mybeacons: function(){
         console.log("Router: mybeacons");
+        alert('Not ready yet');
     },
 
     settings: function(){
@@ -65,23 +97,57 @@ var Router = Backbone.Router.extend({
         this.view = view;
     },
 
-    onRoute: function(){
-        $('#slidemenu li').removeClass('active');
-        $(".navbar-toggle").trigger('click');
-
-        $('#'+Backbone.history.fragment+'-navigation-link').parent().addClass('active');
-    },
-
     showScanner: function(){
         var template = JST['app/scripts/templates/scanner.hbs'];
-
         $('#main').html(template());
     },
 
     showList: function(model){
         var template = JST['app/scripts/templates/list.hbs'];
-
         $('#main').html(template({beacons:model}));  
+    },
+
+    recognizePillar: function(){
+        console.log('Router: recognizePillar');
+
+        $('.navbar').hide();
+        $('#content').attr('style', 'padding-top: 0');
+
+        var template = JST['app/scripts/templates/recognizePillar.hbs'];
+        $('#main').html(template());
+
+        estimote.onUpdate = function(beacons){
+            $.each(beacons, function(index, beacon){
+                if(beacon.distance != undefined){
+                    beacon.distance = (Number(beacon.distance) * 3.28).toFixed(2);
+
+                    console.log(beacon.distance);
+
+                    if(beacon.distance < 0.04){
+                        estimote.onUpdate = function(){};
+                        $('.navbar').show();
+                        $('#content').attr('style', '');
+
+                        var template = JST['app/scripts/templates/registerPillar.hbs'];
+                        var model = {
+                            major: beacon.major,
+                            minor: beacon.minor
+                        };
+                        $('#main').html(template({beacon:model}));
+
+                        $('form').submit(function(e) {
+
+                        });
+
+                        return false; // break
+                    }
+                }
+            });
+        }
+    },
+
+    registerPillar: function(major, minor){
+        console.log('Router: registerPillar');
     }
 
 });
